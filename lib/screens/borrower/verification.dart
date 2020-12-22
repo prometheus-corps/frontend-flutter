a2e005d3f5e5b2e99c016aea52853324741c9c5b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 23:47:29 +0530

    app update

--- lib/screens/borrower/verification.dart
+++ lib/screens/borrower/verification.dart
@@ -47,7 +47,247 @@ class _VerificationScreenState extends State<VerificationScreen> {
           textTheme: Typography.material2018().white,
         ),
         home: SafeArea(
-          child: Scaffold(),
+          child: Scaffold(
+            body: SingleChildScrollView(
+              reverse: false,
+              child: Container(
+                child: Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom * 0.1,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              top: ScreenUtil().setHeight(150),
+                              bottom: ScreenUtil().setHeight(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                      left: ScreenUtil().setWidth(90),
+                                    ),
+                                    child: Text(
+                                      'Register',
+                                      style: GoogleFonts.roboto(
+                                        fontSize: 42,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: ScreenUtil().setWidth(75),
+                                  ),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                          top: ScreenUtil().setHeight(125),
+                                          bottom: ScreenUtil().setHeight(24),
+                                        ),
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              'ACCOUNT INFORMATION',
+                                              style: GoogleFonts.openSans(
+                                                fontWeight: FontWeight.w600,
+                                                fontSize:
+                                                    ScreenUtil().setSp(35),
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Name',
+                                                firstNameController:
+                                                    _nameController,
+                                                type: TextInputType.name,
+                                                obscure: false,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Email',
+                                                firstNameController:
+                                                    _emailController,
+                                                type:
+                                                    TextInputType.emailAddress,
+                                                obscure: false,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Username',
+                                                firstNameController:
+                                                    _userNameController,
+                                                type: TextInputType.name,
+                                                obscure: false,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Phone Number',
+                                                firstNameController:
+                                                    _phoneController,
+                                                type: TextInputType.phone,
+                                                obscure: false,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Bank Account Number',
+                                                firstNameController:
+                                                    _accountController,
+                                                type: TextInputType.phone,
+                                                obscure: false,
+                                              ),
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.symmetric(
+                                                vertical:
+                                                    ScreenUtil().setHeight(24),
+                                              ),
+                                              child: TextFormWidget(
+                                                hint: 'Password',
+                                                firstNameController:
+                                                    _passwordController,
+                                                type: TextInputType
+                                                    .visiblePassword,
+                                                obscure: true,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: ScreenUtil().setHeight(36),
+                                        ),
+                                        child: ButtonTheme(
+                                          minWidth: ScreenUtil().setWidth(1080),
+                                          height: ScreenUtil().setHeight(150),
+                                          child: RaisedButton(
+                                            color: whiteColor,
+                                            child: isWaiting
+                                                ? Container(
+                                                    height: ScreenUtil()
+                                                        .setHeight(120),
+                                                    width: ScreenUtil()
+                                                        .setWidth(120),
+                                                    padding: EdgeInsets.all(8),
+                                                    child:
+                                                        CircularProgressIndicator(
+                                                      strokeWidth: 2,
+                                                      valueColor:
+                                                          AlwaysStoppedAnimation<
+                                                                  Color>(
+                                                              blackColor),
+                                                    ),
+                                                  )
+                                                : Text(
+                                                    'Upload Id Proof',
+                                                    style: GoogleFonts.openSans(
+                                                      fontSize: ScreenUtil()
+                                                          .setSp(48),
+                                                      color: blackColor,
+                                                      fontWeight:
+                                                          FontWeight.w600,
+                                                    ),
+                                                  ),
+                                            onPressed: isWaiting
+                                                ? null
+                                                : () async {
+                                                    _showPicker(context);
+                                                  },
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: ScreenUtil().setHeight(0),
+                                        ),
+                                        child: ButtonTheme(
+                                          minWidth: ScreenUtil().setWidth(1080),
+                                          height: ScreenUtil().setHeight(150),
+                                          child: RaisedButton(
+                                            color: whiteColor,
+                                            child: isWaiting
+                                                ? Container(
+                                                    height: ScreenUtil()
+                                                        .setHeight(120),
+                                                    width: ScreenUtil()
+                                                        .setWidth(120),
+                                                    padding: EdgeInsets.all(8),
+                                                    child:
+                                                        CircularProgressIndicator(
+                                                      strokeWidth: 2,
+                                                      valueColor:
+                                                          AlwaysStoppedAnimation<
+                                                                  Color>(
+                                                              blackColor),
+                                                    ),
+                                                  )
+                                                : Text(
+                                                    'Register',
+                                                    style: GoogleFonts.openSans(
+                                                      fontSize: ScreenUtil()
+                                                          .setSp(48),
+                                                      color: blackColor,
+                                                      fontWeight:
+                                                          FontWeight.w600,
+                                                    ),
+                                                  ),
+                                            onPressed:
+                                                isWaiting ? null : () async {},
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
