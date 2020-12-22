6a1f59d5f2c74a3212508a4fd2c0363318c89b7c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 22:06:58 +0530

    app update

--- lib/screens/borrower/borrowHome.dart
+++ lib/screens/borrower/borrowHome.dart
@@ -77,7 +77,7 @@ class _BorrowHomeScreenState extends State<BorrowHomeScreen> {
                     FontAwesomeIcons.compass,
                     size: ScreenUtil().setSp(65),
                   ),
-                  label: 'Transfer',
+                  label: 'Transactions',
                 ),
                 BottomNavigationBarItem(
                   icon: Icon(
