4a08e385a4dfacfb9d9ef1ffd493ab9155dafeca
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 23:39:46 +0530

    app update

--- lib/screens/borrower/verification.dart
+++ lib/screens/borrower/verification.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class VerificationScreen extends StatefulWidget {
+  @override
+  _VerificationScreenState createState() => _VerificationScreenState();
+}
+
+class _VerificationScreenState extends State<VerificationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
