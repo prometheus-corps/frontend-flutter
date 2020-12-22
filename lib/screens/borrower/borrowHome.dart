09dc421b231a923b4dad9c231db73306f6dd62bb
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 18:47:59 +0530

    app update

---
+++ lib/screens/borrower/borrowHome.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BorrowHomeScreen extends StatefulWidget {
+  @override
+  _BorrowHomeScreenState createState() => _BorrowHomeScreenState();
+}
+
+class _BorrowHomeScreenState extends State<BorrowHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
