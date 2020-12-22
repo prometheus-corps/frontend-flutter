d3c5186619dc40085dca01a3f8c854e20449eabb
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 16:55:00 +0530

    app update

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -2,6 +2,7 @@ import 'package:crowdgain/utilities/constants.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/screenutil.dart';
 
 class RoomScreen extends StatefulWidget {
   @override
