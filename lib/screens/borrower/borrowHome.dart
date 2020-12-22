962c8ccc7ec979038732154c7ac2ca6711877e4e
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 20:57:11 +0530

    app update

--- lib/screens/borrower/borrowHome.dart
+++ lib/screens/borrower/borrowHome.dart
@@ -156,8 +156,8 @@ Eligibility""",
                                 FontAwesomeIcons.registered,
                               ),
                               buildGestureDetector(
-                                """Check EMI
-Rates""",
+                                '''Membership
+Verification''',
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                                 FontAwesomeIcons.registered,
                               ),
@@ -177,8 +177,8 @@ Rates""",
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               buildGestureDetector(
-                                '''Membership
-Verification''',
+                                """Check EMI
+Rates""",
                                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                                 FontAwesomeIcons.registered,
                               ),
