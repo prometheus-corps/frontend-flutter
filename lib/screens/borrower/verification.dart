0a342887120fb0395635113f3a8332749dfc4822
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 23:43:48 +0530

    app update

--- lib/screens/borrower/verification.dart
+++ lib/screens/borrower/verification.dart
@@ -8,6 +8,44 @@ class VerificationScreen extends StatefulWidget {
 class _VerificationScreenState extends State<VerificationScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    int currentIndex = 2;
+    ScreenUtil.init(context,
+        designSize: Size(1080, 2400), allowFontScaling: false);
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RoomScreen(),
+          ),
+        );
+        return Future.value(false);
+      },
+      child: MaterialApp(
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          accentColor: blueColor,
+          primarySwatch: blue,
+          canvasColor: Color(0xFFEFEFF4),
+          iconTheme: IconThemeData(color: blackColor),
+          primaryTextTheme: Typography.material2018().black,
+          textTheme: Typography.material2018().black,
+        ),
+        darkTheme: ThemeData(
+          primarySwatch: black,
+          accentColor: black,
+          brightness: Brightness.dark,
+          canvasColor: blackColor,
+          primaryTextTheme: Typography.material2018().white,
+          textTheme: Typography.material2018().white,
+        ),
+        home: SafeArea(
+          child: Scaffold(),
+        ),
+      ),
+    );
   }
 }
