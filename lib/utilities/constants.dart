c9cced21042e5921bb35ddb1ba8c864a66dfc091
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 11:27:43 +0530

    app update

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,1 +1,9 @@
 import 'package:flutter/material.dart';
+
+// Colors
+final Color splashBgColor = Color(0xFFF8F8F8);
+final Color mainBgColor = Color(0xFFFFFFFF);
+final Color mainTextColor = Color(0xFF000000);
+final Color fadeColor = Colors.grey;
+final Color cardInsideColor = Colors.grey[300];
+final Color blueColor = Color(0xff2B96ED);
