34652499db5b26e8939f41b43ff502e554437a11
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 11:44:40 +0530

    app update

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -7,3 +7,34 @@ final Color blackColor = Color(0xFF000000);
 final Color fadeColor = Colors.grey;
 final Color cardInsideColor = Colors.grey[300];
 final Color blueColor = Color(0xff2B96ED);
+// Material Colors
+const MaterialColor white = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0xFFFFFFFF),
+    100: const Color(0xFFFFFFFF),
+    200: const Color(0xFFFFFFFF),
+    300: const Color(0xFFFFFFFF),
+    400: const Color(0xFFFFFFFF),
+    500: const Color(0xFFFFFFFF),
+    600: const Color(0xFFFFFFFF),
+    700: const Color(0xFFFFFFFF),
+    800: const Color(0xFFFFFFFF),
+    900: const Color(0xFFFFFFFF),
+  },
+);
+const MaterialColor black = const MaterialColor(
+  0xFF000000,
+  const <int, Color>{
+    50: const Color(0xFF000000),
+    100: const Color(0xFF000000),
+    200: const Color(0xFF000000),
+    300: const Color(0xFF000000),
+    400: const Color(0xFF000000),
+    500: const Color(0xFF000000),
+    600: const Color(0xFF000000),
+    700: const Color(0xFF000000),
+    800: const Color(0xFF000000),
+    900: const Color(0xFF000000),
+  },
+);
