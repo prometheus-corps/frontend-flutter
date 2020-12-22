8b7d951f3c91f67da6725a22159e52731fca6dcc
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 16:48:04 +0530

    app update

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class RoomScreen extends StatefulWidget {
+  @override
+  _RoomScreenState createState() => _RoomScreenState();
+}
+
+class _RoomScreenState extends State<RoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
