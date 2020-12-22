1540eefdc2e1beeaa1860590782f9a07c8c9117c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 16:54:29 +0530

    app update

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -8,6 +8,37 @@ class RoomScreen extends StatefulWidget {
 class _RoomScreenState extends State<RoomScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    ScreenUtil.init(context,
+        designSize: Size(1080, 2400), allowFontScaling: false);
+    return WillPopScope(
+      onWillPop: () {
+        SystemNavigator.pop();
+        return Future.value(false);
+      },
+      child: MaterialApp(
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: white,
+          canvasColor: Color(0xFFEFEFF4),
+          iconTheme: IconThemeData(color: blackColor),
+          primaryTextTheme: Typography.material2018().black,
+          textTheme: Typography.material2018().black,
+        ),
+        darkTheme: ThemeData(
+          primarySwatch: black,
+          accentColor: black,
+          brightness: Brightness.dark,
+          canvasColor: blackColor,
+          primaryTextTheme: Typography.material2018().white,
+          textTheme: Typography.material2018().white,
+        ),
+        home: SafeArea(
+          child: Scaffold(),
+        ),
+      ),
+    );
   }
 }
