512ab79f6b4c4c776f8e31e5962e09927325ba3d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 11:55:02 +0530

    app update

--- lib/screens/createLock.dart
+++ lib/screens/createLock.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateLockScreen extends StatefulWidget {
