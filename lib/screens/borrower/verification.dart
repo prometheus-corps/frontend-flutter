187e2387c20b4f9e84526ccc27655eca46b6bb75
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-22 23:45:05 +0530

    app update

--- lib/screens/borrower/verification.dart
+++ lib/screens/borrower/verification.dart
@@ -2,6 +2,7 @@ import 'package:crowdgain/screens/borrower/borrowHome.dart';
 import 'package:crowdgain/utilities/constants.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class VerificationScreen extends StatefulWidget {
   @override
