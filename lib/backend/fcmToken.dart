1cfc5cd9ca64825aaa1d0633cb41be8898866126
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 19:30:15 +0530

    app update

--- lib/backend/fcmToken.dart
+++ lib/backend/fcmToken.dart
@@ -1,6 +1,24 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 Future<String> getFCMToken() async {
+  FirebaseMessaging.getInstance().getToken()
+    .addOnCompleteListener(new OnCompleteListener<String>() {
+        @Override
+        public void onComplete(@NonNull Task<String> task) {
+          if (!task.isSuccessful()) {
+            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+            return;
+          }
+
+          // Get new FCM registration token
+          String token = task.getResult();
+
+          // Log and toast
+          String msg = getString(R.string.msg_token_fmt, token);
+          Log.d(TAG, msg);
+          Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+    });
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   _firebaseMessaging.configure();
   String tokenKey = '';
