645d67dd43df62bc5f42aafcd9b8aabf5fdf6cd8
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 22:52:16 +0530

    app update

--- lib/screens/borrower/waitingScreen.dart
+++ lib/screens/borrower/waitingScreen.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class WaitingRoomScreen extends StatefulWidget {
+  @override
+  _WaitingRoomScreenState createState() => _WaitingRoomScreenState();
+}
+
+class _WaitingRoomScreenState extends State<WaitingRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
