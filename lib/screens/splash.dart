0a0443c1bda6ee1341560761286eca2299dd38ca
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 12:47:34 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -4,8 +4,6 @@ import 'package:crowdgain/utilities/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-String routeKey = 'createLock';
-
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
