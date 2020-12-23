11b0892eb007790cb578d3037279c2661cfb4115
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 23:29:01 +0530

    app update

--- lib/screens/borrower/waitingScreen.dart
+++ lib/screens/borrower/waitingScreen.dart
@@ -1,6 +1,7 @@
 import 'package:crowdgain/utilities/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WaitingRoomScreen extends StatefulWidget {
   @override
