27bee8f185749bb60ee352c3a591714d7969697f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 13:13:42 +0530

    app update

--- lib/backend/fcmToken.dart
+++ lib/backend/fcmToken.dart
@@ -4,6 +4,7 @@ Future<String> getFCMToken() async {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   _firebaseMessaging.configure();
   String tokenKey = '';
+  print('Token as :');
   await _firebaseMessaging.getToken().then((String token) {
     assert(token != null);
     tokenKey = token;
