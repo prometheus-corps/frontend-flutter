dffb11d5238f0e5dfaab73dc2fc5df9878124d9c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-23 23:30:03 +0530

    app update

--- lib/screens/borrower/waitingScreen.dart
+++ lib/screens/borrower/waitingScreen.dart
@@ -1,3 +1,4 @@
+import 'package:crowdgain/screens/borrower/borrowHome.dart';
 import 'package:crowdgain/utilities/constants.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
