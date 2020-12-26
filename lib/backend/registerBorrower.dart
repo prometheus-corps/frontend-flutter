3d2169a03b8c0bb7e7d786298f4462c9579fc65b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-26 12:31:35 +0530

    app update

--- lib/backend/registerBorrower.dart
+++ lib/backend/registerBorrower.dart
@@ -1,22 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<int> registerBorrower(String name, String email, String username, int phoneNumber, String bankId, String password ) {
+registerBorrower(String name, String email, String username, String phoneNumber,
+    String bankId, String password) {
   final _auth = FirebaseAuth.instance;
   String detailedBody =
-        ' Name: $name \n Email : $email \n Contact Number: $phoneNumber \n UserName: $username\n Bank Id: $bankId \n password: $password';
-    if (await canLaunch(
-            'mailto:info@qwings.in?subject=Register=New&body=$detailedBody%20plugin') &&
-        studentName != '' &&
-        parentMailId != '' &&
-        parentName != '' &&
-        parentnumber != '') {
-      try {
-       
-      } catch (e) {
-      return 0;
-      }
-    } else {
+      ' Name: $name \n Email : $email \n Contact Number: $phoneNumber \n UserName: $username\n Bank Id: $bankId \n password: $password';
+  print(detailedBody);
+  if (phoneNumber.length == 10 &&
+      name != '' &&
+      email != '' &&
+      password != '' &&
+      bankId != '') {
+    try {} catch (e) {
       return 0;
     }
-  
+  } else {
+    return 0;
+  }
 }
